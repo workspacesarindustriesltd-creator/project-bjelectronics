b7ea8f7deb0335e84f5b8649323f6f9a2ce47d20
workspace.sarindustriesltd <workspace.sarindustriesltd@gmail.com>
2026-08-06 03:10:24 +0600

    Switch production admin entry to enterprise control center

--- apps/admin/main.jsx
+++ apps/admin/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { AdminPlatform as AdminPortal } from "../../src/admin/AdminPlatform.jsx";
-import "../../src/admin/admin-portal.css";
-import "../../src/admin-media.css";
+import { AdminEnterprise } from "../../src/admin-enterprise/AdminEnterprise.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AdminPortal />
+    <AdminEnterprise />
   </React.StrictMode>,
 );
